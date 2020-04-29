59c3f03cb00297404c028fb8813a0791749febd3
Kate Glekh <Detlaf@users.noreply.github.com>
2020-04-29 10:28:02 +0300

    Added pdf support and subdirectories

--- ya_vision.py
+++ ya_vision.py
@@ -1,13 +1,11 @@
-import os
-import json
 import base64
+import json
 import requests
 
-oauth = ''
-catalog_id = 'b1gh30snh01dcojq8ub3'
-image_path = 'img/keep_calm.png'
-image_dir = 'img'
-pdf_dir = 'pdf'
+def get_oauth(oauth_path):
+    f = open(oauth_path, "r")
+    oauth = f.read()
+    return oauth
 
 def get_iam_token(iam_url, oauth):
     response = requests.post(url=iam_url, json={"yandexPassportOauthToken": oauth})
@@ -52,41 +50,3 @@ def extract_pdf(vision_url, iam_token, catalog_id, pdf_data):
             }
         ]})
     return response.text
-
-def main():
-    iam_url = 'https://iam.api.cloud.yandex.net/iam/v1/tokens'
-    vision_url = 'https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze'
-
-    iam_token = get_iam_token(iam_url, oauth)
-    res_file = open("results.txt", "w")
-    pdf_res_file = open("results_pdf.txt", "w")
-
-    for filename in os.listdir(image_dir):
-        with open(os.path.join(image_dir, filename), "rb") as f:
-            res_file.write(filename)
-            res_file.write('\n')
-            image_data = encode_file(f)
-        text_from_image = extract_image(vision_url, iam_token, catalog_id, image_data)
-        json_text = json.loads(text_from_image)
-        for i in json_text['results'][0]['results'][0]['textDetection']['pages'][0]['blocks']:
-            for j in i['lines']:
-                for k in j['words']:
-                    res_file.write(k['text'] + ' ')
-            res_file.write('\n')
-
-    for filename in os.listdir(pdf_dir):
-        with open(os.path.join(pdf_dir, filename), "rb") as f:
-            pdf_res_file.write(filename)
-            pdf_res_file.write('\n---------------------\n')
-            pdf_data = encode_file(f)
-        text_from_pdf = extract_pdf(vision_url, iam_token, catalog_id, pdf_data)
-        json_text = json.loads(text_from_pdf)
-        for i in json_text['results'][0]['results'][0]['textDetection']['pages']:
-            for j in i['blocks']:
-                for k in j['lines']:
-                    for z in k['words']:
-                        pdf_res_file.write(z['text'] + ' ')
-                pdf_res_file.write('\n')
-
-if __name__ == '__main__':
-    main()
